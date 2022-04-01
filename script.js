280e33a51ae6f65f215b6ce327c5ee19b92be394
FrostandIce <brodypen7987@gmail.com>
2022-03-31 21:45:06 -0500

    Done and rolled... rock paper scissors yo.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-alert("hello");
+const moveStatus = document.getElementById("moveS");
+const scoreCount = document.getElementById("scoreC");
+let yourScore = 0;
+let enemyScore = 0;
+let won = false;
+function getRandomChoice() {
+  return Math.floor(Math.random() * 3);
+}
+function rockPaperScissors(choice) {
+  if (!won) {
+    const AiChoosed = getRandomChoice();
+    if (AiChoosed == 0) {
+      moveStatus.innerHTML = "Draw!";
+    }
+    if (AiChoosed == 1) {
+      moveStatus.innerHTML = "You won!";
+      yourScore++;
+    }
+    if (AiChoosed == 2) {
+      moveStatus.innerHTML = "You lost!";
+      enemyScore++;
+    }
+    scoreCount.innerHTML = yourScore + " - " + enemyScore;
+    if (yourScore >= 5) {
+      moveStatus.innerHTML = "You won the game!";
+      won = true;
+    }
+    if (enemyScore >= 5) {
+      moveStatus.innerHTML = "You lost the game!";
+      won = true;
+    }
+  }
+}
